8029e8447276349a8f8f938525538c7d23362a18
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-07 15:49:11 +0200

    add function index

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Movies;
+
 class PageController extends Controller
 {
-    //
+    public function index() {
+      $movies = Movies::all();
+      dump($movies);
+      return view('movies');
+    }
 }
